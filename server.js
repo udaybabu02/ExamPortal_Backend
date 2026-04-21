6ba59e8d6a4363950a87eca49d69d66679b074e7
Uday Babu <udayyuddamsetti@gmail.com>
2026-04-21 15:39:34 +0530

    Update: Added SSL required for Aiven DB connection

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
+    ssl: { rejectUnauthorized: false }, // <--- REQUIRED FOR AIVEN CLOUD DB
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
